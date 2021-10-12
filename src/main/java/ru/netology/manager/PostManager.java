65ef19067c28fb84f354642219786f97a9081865
Viktoria <vikitoga@gmail.com>
2021-10-13 00:07:29 +0300

    Class PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] postItem;
+
+    public Post[] searchPost(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void deletePost(int ownerId, int postId) {
+
+    }
+}
